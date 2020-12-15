da834d28794aae8704d4fdfec3d20f4a1f6a4559
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2020-12-15 18:05:01 +0700

    convolution on multi-channels

--- Convolution.cpp
+++ Convolution.cpp
@@ -14,19 +14,28 @@ void Convolution::SetKernel(vector<float> kernel, int kWidth, int kHeight)
 
 int Convolution::DoConvolution(const Mat& sourceImage, Mat& destinationImage)
 {
+	if (sourceImage.data == NULL) return 1;
+
+	if (_kernelHeight % 2 == 0 || _kernelWidth % 2 == 0) return 1;
+
 	int topLeftX = _kernelWidth / 2;
 	int topLeftY = _kernelHeight / 2;
 	int botRightX = sourceImage.cols - topLeftX - 1;
 	int botRightY = sourceImage.rows - topLeftY - 1;
 
-	destinationImage = Mat(botRightY - topLeftY + 1, botRightX - topLeftX + 1, CV_8UC1);
+	if (sourceImage.channels() == 1)
+		destinationImage = Mat(botRightY - topLeftY + 1, botRightX - topLeftX + 1, CV_8UC1);
+	else if (sourceImage.channels() == 3)
+		destinationImage = Mat(botRightY - topLeftY + 1, botRightX - topLeftX + 1, CV_8UC3);
+	else
+		return 1;
 
 	int* offset = new int[_kernelHeight * _kernelWidth];
 	auto ptr = offset;
 	for (int y = -_kernelHeight / 2; y <= _kernelHeight / 2; ++y)
 		for (int x = -_kernelWidth / 2; x <= _kernelWidth / 2; ++x)
 		{
-			*ptr = sourceImage.cols * y + x;
+			*ptr = (sourceImage.cols * y + x) * sourceImage.channels();
 			++ptr;
 		}
 
@@ -35,15 +44,19 @@ int Convolution::DoConvolution(const Mat& sourceImage, Mat& destinationImage)
 	for (int i = topLeftY; i <= botRightY; ++i)
 		for (int j = topLeftX; j <= botRightX; ++j)
 		{
-			float val = 0;
-			int sPos = sourceImage.cols * i + j;
-			for (int k = 0; k < _kernelHeight * _kernelWidth; ++k)
-				val += sData[sPos + offset[k]] * _kernel[k];
+			int sPos = (sourceImage.cols * i + j) * sourceImage.channels();
+			for (int c = 0; c < sourceImage.channels(); ++c)
+			{
+				float val = 0;
+				for (int k = 0; k < _kernelHeight * _kernelWidth; ++k)
+					val += sData[sPos + offset[k]] * _kernel[k];
 
-			if ((int)val > 255) val = 255;
-			if ((int)val < 0) val = 0;
-			*dData = val;
-			dData += 1;
+				if ((int)val > 255) val = 255;
+				if ((int)val < 0) val = 0;
+				*dData = val;
+				++dData;
+				++sPos;
+			}
 		}
 
 	delete[] offset;
